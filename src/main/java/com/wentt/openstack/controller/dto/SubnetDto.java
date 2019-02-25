326988ced098aed366aee580e6eff28b95ed4c21
kaixuan <kaixuanlu1992@163.com>
2019-02-25 12:51:08 +0800

    创建子网

---
+++ src/main/java/com/wentt/openstack/controller/dto/SubnetDto.java
@@ -0,0 +1,15 @@
+package com.wentt.openstack.controller.dto;
+
+import lombok.Data;
+
+@Data
+public class SubnetDto {
+    private String name;
+    private String networkId;
+    private String tenantId;
+    private String cidr;
+    private String ipVersion;
+    private String gateway;
+    private String addressPoolStart;
+    private String addressPoolEnd;
+}
