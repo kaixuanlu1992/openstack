f68db8e126787f9409479eb7a4ff5e042a46e28f
kaixuan <kaixuanlu1992@163.com>
2019-02-25 16:39:15 +0800

    1、swagger 2、接口拆分

--- src/main/java/com/wentt/openstack/controller/NetworkController.java
+++ src/main/java/com/wentt/openstack/controller/NetworkController.java
@@ -10,6 +10,8 @@ import com.wentt.openstack.controller.vo.SubnetVo;
 import com.wentt.openstack.service.FlavorService;
 import com.wentt.openstack.service.NetworkService;
 import com.wentt.openstack.service.ServerService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
@@ -18,6 +20,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 @RestController
+@Api(tags = "网络接口")
 public class NetworkController {
     @Value("${network.type}")
     private List<String> networkTypeList;
@@ -30,67 +33,51 @@ public class NetworkController {
 
 
     @GetMapping("/network/type/list")
+    @ApiOperation("获取网络类型列表")
     public List<String> getNetworkTypeList(HttpServletResponse response) {
         response.setHeader("Access-Control-Allow-Origin", "*");
         return networkTypeList;
     }
     @GetMapping("/network/list")
+    @ApiOperation("获取网络列表")
     public List<NetworkVo> getNetworkList(){
         return networkService.getNetworkList();
     }
 
     @PostMapping("/network")
+    @ApiOperation("创建网络")
     public String createNetwork(@RequestBody NetworkDto dto){
         return networkService.createNetwork(dto);
     }
 
     @PutMapping("/network")
+    @ApiOperation("更新网络")
     public String updateNetwork(){
         return null;
     }
 
     @DeleteMapping("/network")
+    @ApiOperation("删除网络")
     public Boolean deleteNetwork(String networkId){
         return networkService.deleteNetwork(networkId);
     }
 
     @GetMapping("/subnet/list")
+    @ApiOperation("获取子网列表")
     public List<SubnetVo> getSubnetList(@RequestParam("networkId")String networkId){
         return networkService.getSubnetList(networkId);
     }
 
     @PostMapping("/subnet")
+    @ApiOperation("创建子网")
     public String createSubnet(SubnetDto subnetDto){
         return networkService.createSubnet(subnetDto);
     }
 
     @DeleteMapping("/subnet/")
+    @ApiOperation("删除子网")
     public void deleteSubnet(List<String> subnetIdList){
          networkService.deleteSubnet(subnetIdList);
     }
 
-    @GetMapping("/server/list")
-    public List<ServerVo> getServerList(@RequestParam("networkId")String networkId){
-        return serverService.getServerList(networkId);
-    }
-
-    @PostMapping("/server")
-    public String createServer(@RequestBody ServerDto serverDto){
-        return serverService.createServer(serverDto);
-    }
-
-    @PutMapping("/server")
-    public String updateServer(){
-        return null;
-    }
-
-    @DeleteMapping("/server")
-    public Boolean deleteServer(String serverId){
-        return serverService.deleteServer(serverId);
-    }
-
-    @GetMapping("/flavor/list")
-    public List<FlavorVo> getFlavorList(){
-        return flavorService.getFlavorList();
-    }
 }
---
+++ src/main/java/com/wentt/openstack/controller/ServerController.java
@@ -0,0 +1,46 @@
+package com.wentt.openstack.controller;
+
+import com.wentt.openstack.controller.dto.ServerDto;
+import com.wentt.openstack.controller.vo.FlavorVo;
+import com.wentt.openstack.service.FlavorService;
+import com.wentt.openstack.service.ServerService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@Api(tags = "实例接口")
+public class ServerController {
+
+    @Autowired
+    private FlavorService flavorService;
+    @Autowired
+    private ServerService serverService;
+
+    @PostMapping("/server")
+    @ApiOperation("创建实例")
+    public String createServer(@RequestBody ServerDto serverDto){
+        return serverService.createServer(serverDto);
+    }
+
+    @PutMapping("/server")
+    @ApiOperation("更新实例")
+    public String updateServer(){
+        return null;
+    }
+
+    @DeleteMapping("/server")
+    @ApiOperation("删除实例")
+    public Boolean deleteServer(String serverId){
+        return serverService.deleteServer(serverId);
+    }
+
+    @GetMapping("/flavor/list")
+    @ApiOperation("flavor列表")
+    public List<FlavorVo> getFlavorList(){
+        return flavorService.getFlavorList();
+    }
+}
